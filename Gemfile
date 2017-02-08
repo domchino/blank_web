ad2ed7182ec30eeb7abc8a1280c4f12127f9a1f2
Dom Chino <blankcreativegroup@gmail.com>
2017-02-08 13:09:27 -0500

    Created Pages controller. Setup routes. Converted erb to Haml. installed haml gem and html2haml gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+
+gem 'html2haml', '~> 2.1'
+gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'fog', '~> 1.38'
 gem 'carrierwave', '~> 1.0'
 gem 'mini_magick', '~> 4.6'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+
+  get '/home' => 'pages#home'
+
+  get '/about' => 'pages#about'
+
+  get '/community' => 'pages#community'
+
+  get '/profile' => 'pages#profile'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
