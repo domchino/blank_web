0cedf4caf73ca0dc3dfbcc5cbebbf4bac7bcc814
Dom Chino <blankcreativegroup@gmail.com>
2017-02-12 21:11:31 -0500

    Users can only edit and delete their own posts. Updated Post Styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :owned_post, only: [:edit, :update, :destroy]  
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -12,8 +14,10 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
+      flash[:success] = "Your post has been created!"
       redirect_to @post
     else
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
       render 'new'
     end
   end
@@ -46,5 +50,12 @@ class PostsController < ApplicationController
 
   def find_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to home_path
+    end
   end
 end
